c760775421009cf20084a90d94de4f87dc10d9ef
gadrian99 <adriangarcia1999@yahoo.com>
2020-07-02 18:04:42 -0400

    default axios settings and api fetch call setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 import App from './components/App';
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:3001'
 
 ReactDOM.render(
   <App />,
